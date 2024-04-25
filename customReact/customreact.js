f8bca598e8521c1e0ba5918eeccd96e2d8272c59
Sourav Dishri <souravdishri@gmail.com>
2024-04-25 13:20:20 +0530

    custom react

---
+++ customReact/customreact.js
@@ -0,0 +1,35 @@
+//here we are creating both customRender method and custom reactElement object
+
+//In react, under the hood, the function is converted into object and render it by its own method
+
+function customRender(reactElement, container) {
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement, mainContainer)
