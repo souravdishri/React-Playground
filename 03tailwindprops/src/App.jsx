8fc293c822e243c61ddf142daa387451010e3d19
Sourav Dishri <souravdishri@gmail.com>
2024-04-26 20:44:27 +0530

    add 03tailwindprops

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from "./components/Card.jsx";
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "sourav",
+    age: 25
+  }
+  let newArr = [1, 2, 3]
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Hello React! | Tailwind</h1>
+
+    {/* <Card username="Hello React!" obj={myobj}/> //this will work */}
+    {/* <Card username="Hello React!" obj={newArr}/> //this will work */}
+
+    <Card username="Hello React!" btnText="click me" />
+    <Card username="sourav" />
+
+    </>
+  )
+}
+
+export default App
