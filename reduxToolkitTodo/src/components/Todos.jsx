a50e78d2a55a5c7d5c9c9c4e1c90f688635e9f81
Sourav Dishri <souravdishri@gmail.com>
2024-05-19 19:02:08 +0530

    fix indentation

--- reduxToolkitTodo/src/components/Todos.jsx
+++ reduxToolkitTodo/src/components/Todos.jsx
@@ -2,17 +2,17 @@
 
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {removeTodo} from '../features/todo/todoSlice'
+import { removeTodo } from '../features/todo/todoSlice'
 
 function Todos() {
-    const todos = useSelector(state => state.todos)
-    const dispatch = useDispatch()
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
 
   return (
     <>
-    <br />
-    <div><p className='text-gray-300'>Todos</p></div>
-    <ul className="list-none">
+      <br />
+      <div><p className='text-gray-300'>Todos</p></div>
+      <ul className="list-none">
         {todos.map((todo) => (
           <li
             className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
@@ -20,7 +20,7 @@ function Todos() {
           >
             <div className='text-white'>{todo.text}</div>
             <button
-             onClick={() => dispatch(removeTodo(todo.id))}
+              onClick={() => dispatch(removeTodo(todo.id))}
               className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
             >
               <svg
