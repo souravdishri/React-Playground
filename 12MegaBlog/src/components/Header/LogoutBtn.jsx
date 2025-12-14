377cd30577f8238da69fdd1a422ac2db99652eeb
Sourav Dishri <souravdishri@gmail.com>
2025-12-15 01:08:34 +0530

    add useNavigate

--- 12MegaBlog/src/components/Header/LogoutBtn.jsx
+++ 12MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -4,15 +4,18 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth.js";
 import { logout } from "../../store/authSlice.js";
+import { useNavigate } from "react-router-dom";
 
 function LogoutBtn() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     
     const logoutHandler = () => {
         authService.logout()
             .then(() => {
                 // we are logging out from appwrite
                 dispatch(logout()); // we are updating the redux store
+                navigate("/login"); // redirecting to login page
             })
             .catch((error) => { //handling error if any
                 console.error("Logout failed:", error);
