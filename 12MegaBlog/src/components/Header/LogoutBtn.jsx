709467f522255a830cb24dec1b04fc02a4459789
Sourav Dishri <souravdishri@gmail.com>
2025-12-15 00:54:09 +0530

    add catch block

--- 12MegaBlog/src/components/Header/LogoutBtn.jsx
+++ 12MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -1,23 +1,32 @@
 //LogoutBtn.jsx
 
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import authService from '../../appwrite/auth.js'
-import { logout } from '../../store/authSlice.js'
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth.js";
+import { logout } from "../../store/authSlice.js";
 
 function LogoutBtn() {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    
     const logoutHandler = () => {
-        authService.logout().then(() => {
-            dispatch(logout())
-        })
-    }
+        authService.logout()
+            .then(() => {
+                // we are logging out from appwrite
+                dispatch(logout()); // we are updating the redux store
+            })
+            .catch((error) => { //handling error if any
+                console.error("Logout failed:", error);
+            });
+    };
+
     return (
         <button
-            className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+            className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
             onClick={logoutHandler}
-        >Logout</button>
-    )
+        >
+            Logout
+        </button>
+    );
 }
 
-export default LogoutBtn
+export default LogoutBtn;
