c5094ed544d2a148f3fcce793b0e106d3ad63cc2
Sourav Dishri <souravdishri@gmail.com>
2025-12-19 23:17:02 +0530

    add comment

--- 12MegaBlog/src/components/Button.jsx
+++ 12MegaBlog/src/components/Button.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function Button({
+    // children prop means whatever is passed between the opening and closing tags of the Button component,
+    // it will be rendered inside the button (it may be text or other elements)
     children,
     type = "button",
     bgColor = "bg-blue-600",
