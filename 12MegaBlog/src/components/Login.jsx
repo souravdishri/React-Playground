a1aeac8eca23a549f11cc950cd98b996f77a31b1
Sourav Dishri <souravdishri@gmail.com>
2025-12-19 23:17:36 +0530

    add comment

--- 12MegaBlog/src/components/Login.jsx
+++ 12MegaBlog/src/components/Login.jsx
@@ -13,14 +13,15 @@ function Login() {
     const [error, setError] = useState("")
 
     const login = async (data) => {
+        console.log("Data:", data);
         setError("")
         try {
-            const session = await authService.login(data)
-            if (session) {
-                const userData = await authService.getCurrentUser()
-                if (userData) dispatch(authLogin(userData));
-                //By the use of navigate we programmatically send the user to root, but in link user need to click
-                navigate("/")       
+            const session = await authService.login(data) //creating a session, it returns a promise
+            if (session) {  //if session is created successfully
+                const userData = await authService.getCurrentUser() // getting current user data through appwrite
+                if (userData) dispatch(authLogin(userData)); // dispatching the user data to the store
+                //By the use of navigate we programmatically send the user to root, but in `<Link>` user need to click it
+                navigate("/")
             }
         } catch (error) {
             setError(error.message)
@@ -37,7 +38,9 @@ function Login() {
                         <Logo width="100%" />
                     </span>
                 </div>
+
                 <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+
                 <p className="mt-2 text-center text-base text-black/60">
                     Don&apos;t have any account?&nbsp;
                     <Link
@@ -47,13 +50,22 @@ function Login() {
                         Sign Up
                     </Link>
                 </p>
+
                 {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+
+                {/* Here we use react-hook-form for form handling */}
+                {/* `handleSubmit` is a function provided by react-hook-form and inside that we pass `login` function which is the function we defined above */}
                 <form onSubmit={handleSubmit(login)} className='mt-8'>
                     <div className='space-y-5'>
+                        
+                        {/* here we use input component which we made */}
                         <Input
                             label="Email: "
                             placeholder="Enter your email"
                             type="email"
+                            //register function from react-hook-form, we must have to use spread operator like `{...register()}` otherwise if we are using this 'register' in any another input it will replace the entire value
+                            //it takes ('two arguments') can take more as well, first is the ('name of the input field') & second is the ('validation rules')
+                            //name of the input & it should be unique, (it contains key, options), (because the final object which will be spread, the `data` like in `.login(data)` will be based on this)
                             {...register("email", {
                                 required: true,
                                 validate: {
@@ -67,7 +79,7 @@ function Login() {
                             label="Password: "
                             type="password"
                             placeholder="Enter your password"
-                            {...register("password", {//name of the input & it should be unique, (it contains key, options)
+                            {...register("password", {
                                 required: true,
                             })}
                         />
@@ -75,6 +87,7 @@ function Login() {
                             type="submit"
                             className="w-full"
                         >Sign in</Button>
+
                     </div>
                 </form>
             </div>
